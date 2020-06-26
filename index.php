7618394c9f6db591bf1fb53ffa0192836bda5a69
Ha Dong <congle7997@gmail.com>
2020-06-26 17:43:10 +0700

    ss

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
 	$url = 'https://sinhvien.ictu.edu.vn/hoat-dong/diem-ca-nhan/';
 	$id = $_GET['id'];
 	$page = file_get_html($url.$id.'.html');
+	$status = substr($page, strpos($page, 'stt:"') + 5, 3);
 
+	if ($status == 'err') {
+		$json = '{"status" : "err"}';
+		die($json);
+	}
 	$name = $page->find('h1', 0)->plaintext;
 	$class = $page->find('h2', 0)->plaintext;
 	$temp = $page->find('h3', 0)->find('span', 0)->plaintext;
 	$true = $page->find('h3', 1)->find('span', 0)->plaintext;
 	$wait = $page->find('h3', 1)->find('span', 1)->plaintext;
-	$img = $page->find('img', 2)->src;
+	$image = $page->find('img', 2)->src;
 	$start = strpos($page, 'list');
 	$end = strpos($page, '],list_emu') - $start;
 	$data = substr($page, $start, $end);
@@ -26,12 +31,13 @@
 		array_push($list_event, new Event($event_id, $event_name));
 	}
 
-	$arr = array('name' => $name,
+	$arr = array('status' => $status,
+		'name' => $name,
 		'class' => $class,
 		'temp' => $temp,
 		'true' => $true,
 		'wait' => $wait,
-		'img' => $img,
+		'image' => $image,
 		'list_event' => $list_event);
 
 	$json = stripslashes(json_encode($arr, JSON_UNESCAPED_UNICODE));
